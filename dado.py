40303e5a22f985aa9c1e5c21fb36e02dcd6552cc
beart75 <beart75@gmail.com>
2024-07-03 14:21:45 +0200

    refactorizar clase dado

--- dado.py
+++ dado.py
@@ -7,6 +7,15 @@ SCRIPT DE SIMULACION DE DADOS
 
 """
 print(INTERFAZ)
-numero_caras = int(input("¿De cuantas caras quieres el dado?"))
+
+Class Dado:
+    def __init__(seft, numero_caras:int)
+        seft.numero_caras = numero_caras
+    def tirada(seft):
+        return random.randint(1 , seft.numero_caras)
+
+numero_caras = int(input("¿De cuantas caras quieres el dado? "))
 dado =random.randint(1,numero_caras)
-print(f" El resultado de tu tirada es {dado}")
+tirada = dado.tirada()
+
+print(f" El resultado de tu tirada es {tirada}")
