4e21b63aec527c09fbdaecec61e8a84c454a2519
beart75 <beart75@gmail.com>
2024-07-03 14:09:06 +0200

    mejora

--- dado.py
+++ dado.py
@@ -1,4 +1,12 @@
 import random
 
-numero_aleatorio =random.randint(1,6)
-print(numero_aleatorio)
+INTERFAZ ="""
+#############################
+SCRIPT DE SIMULACION DE DADOS
+#############################
+
+"""
+print(INTERFAZ)
+numero_caras = int(input("¿De cuantas caras quieres el dado?"))
+dado =random.randint(1,numero_caras)
+print(f" El resultado de tu tirada es {dado}")
